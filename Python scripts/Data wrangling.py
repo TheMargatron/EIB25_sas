83c674584aa0fa3ade6da6090cfccfd8a1c0501e
Margaret Bolton <m.a.bolton@hotmail.co.uk>
2025-07-24 17:12:26 +0100

    Starting python transition

---
+++ Python scripts/Data wrangling.py
@@ -0,0 +1,11 @@
+# =============================================================================
+# Title:        Surfers Against Sewage
+# Description:  wrangles data from water company APIs for ArcGIS dashboard
+# Author:       Margaret Bolton <mb804 (at) exeter.ac.uk>
+# Created:      2025-07-24
+# Last updated: 2025-07-24
+# Dependencies: Python 3.10+
+#               pandas, requests, geopandas, shapely
+# Inputs:       aggregated static data from water company APIs
+# Outputs:      ArcGIS dashboard
+# =============================================================================
